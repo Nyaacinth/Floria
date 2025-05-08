304625771fe63e2934b9b1f8489811f30b9dbe44
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2025-05-08 15:26:26 +0800

    Fix typings

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
-/** @type {import("prettier").Config} */
+/** @type {import("prettier").Config & import("prettier-plugin-tailwindcss").PluginOptions}*/
 export default {
     printWidth: 120,
     tabWidth: 4,
