2279261a30680a183fd05b790f3707c303f30581
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2025-05-08 15:23:01 +0800

    Fix the issue that react-compiler doesn't apply to hooks in [jt]s files

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react-swc"
+import { extname } from "node:path"
 import macros from "unplugin-macros/vite"
 import { defineConfig } from "vite"
 import babel from "vite-plugin-babel"
@@ -8,14 +9,16 @@ export default defineConfig({
     plugins: [
         macros(),
         babel({
-            filter: /\.[jt]sx$/,
+            filter: /\.[jt]sx?$/,
             babelConfig: {
                 plugins: [
                     ["@babel/plugin-syntax-typescript", { isTSX: true }],
                     ["babel-plugin-react-compiler", {}]
                 ]
             },
-            loader: (fpath) => (fpath.endsWith(".tsx") ? "tsx" : "jsx")
+            loader: (fpath) => {
+                return extname(fpath).replace(/^\./, "") as "js" | "ts" | "jsx" | "tsx"
+            }
         }),
         react(),
         tailwindcss()
