4ce5f19a90035edfc34824406f0cec9b4e9010bd
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2025-07-08 11:22:15 +0800

    Add decorum

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,25 @@
 use tauri::Manager;
+use tauri_plugin_decorum::WebviewWindowExt;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_decorum::init())
         .setup(|app| {
+            let main_window = app.get_webview_window("main").unwrap();
+
             #[cfg(debug_assertions)]
-            app.get_webview_window("main").unwrap().open_devtools();
+            main_window.open_devtools();
+
+            // Create a custom titlebar for main window
+            // On Windows this hides decoration and creates custom window controls
+            // On macOS it needs hiddenTitle: true and titleBarStyle: overlay
+            main_window.create_overlay_titlebar().unwrap();
+
+            // Set a custom inset to the traffic lights
+            #[cfg(target_os = "macos")]
+            main_window.set_traffic_lights_inset(12.0, 16.0).unwrap();
+
             Ok(())
         })
         .run(tauri::generate_context!())
