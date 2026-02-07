bd6b395584e5e25d5f6de36cc812eabd5bdd53ea
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2026-02-07 16:37:51 +0800

    Declare types of electron-versions

---
+++ packages/electron-versions/index.d.ts
@@ -0,0 +1,9 @@
+export function getElectronVersions(): { [packageName: string]: string }
+
+export function getChromeVersion(): string
+
+export function getChromeMajorVersion(): number
+
+export function getNodeVersion(): string
+
+export function getNodeMajorVersion(): number
